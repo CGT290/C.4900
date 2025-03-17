ec22ef90b66768b7649e80eb27ceeee266b22875
Peter Joseph <peterjoseph571@gmail.com>
2025-03-16 23:06:25 -0400

    new config file

--- Transit_APP/app/index.tsx
+++ Transit_APP/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <ScrollView>
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
+    </ScrollView>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        color: "red",
+
+  },
+  Text: {
+    color: "red",
+    
+  }
+
+}
+);
